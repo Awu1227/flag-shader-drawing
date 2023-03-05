c447e4feb933776d949911b009b8e03dce82908b
orange <2497436726@qq.com>
2023-03-05 16:44:18 +0800

    :tada:project submit

---
+++ src/shaders/test/fragment.glsl
@@ -0,0 +1,9 @@
+precision mediump float;
+
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+void main() {
+		vec4 textureColor = texture2D(uTexture, vUv);
+		gl_FragColor = textureColor;
+}
---
+++ src/shaders/test/vertex.glsl
@@ -0,0 +1,26 @@
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
+
+
+attribute vec3 position;
+
+attribute vec2 uv;
+
+varying vec2 vUv;
+
+void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+		modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;	
+		modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;	
+
+		modelPosition.y *= 0.75;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+		vUv = uv;
+}
